b14d551902c0e75fd63392a4056ca6cb9bbd2b92
Mikkel Vinther Christiansen <mikkel_vinther@hotmail.com>
2020-09-07 20:33:04 +0200

    Updates

---
+++ precache-manifest.7dd2b3bc03283e9cb180b3ff48e980fb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4181bd8e36b0d98b0f012ecb56a5c2b",
+    "url": "/ibm-cic_virtual_whiteboard/index.html"
+  },
+  {
+    "revision": "d0f121f15ac9a1f9022e",
+    "url": "/ibm-cic_virtual_whiteboard/static/css/main.4211fe40.chunk.css"
+  },
+  {
+    "revision": "acbd2c91cd9fb8caa23d",
+    "url": "/ibm-cic_virtual_whiteboard/static/js/2.cdf4ac0b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ibm-cic_virtual_whiteboard/static/js/2.cdf4ac0b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d0f121f15ac9a1f9022e",
+    "url": "/ibm-cic_virtual_whiteboard/static/js/main.38c539b2.chunk.js"
+  },
+  {
+    "revision": "f418b6103c8b31bc7698",
+    "url": "/ibm-cic_virtual_whiteboard/static/js/runtime-main.0360ec71.js"
+  },
+  {
+    "revision": "658bc24a8adba08fba775a575ec0cef4",
+    "url": "/ibm-cic_virtual_whiteboard/static/media/cute-kitten.658bc24a.jpg"
+  }
+]);
